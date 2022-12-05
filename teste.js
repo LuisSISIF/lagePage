4256fc4a26047e543a857e874259855c5f8201bf
LuisSISIF <102630643+LuisSISIF@users.noreply.github.com>
2022-12-05 14:15:44 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,13 @@
+var x1 = true;
+        var x2 = true;
+        var x3 = true;
+        var t = 0;
+        
+        
+            switch(x1 == true){
+                case 0:
+                    t2=false;
+                    t3=false;
+                    console.log(t1,t2,t3);
+                    break;
+        }
